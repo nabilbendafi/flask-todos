f783c2debe710c520c73722e1d19831ec5867557
Nabil BENDAFI <nabil@bendafi.fr>
2021-06-12 23:43:24 +0200

    feat: add todo submission form

--- todos/src/todos.py
+++ todos/src/todos.py
@@ -1,10 +1,13 @@
 #!/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def todos():
+    if request.method == 'POST':
+        app.logger.debug(request)
+        app.logger.debug(request.form)
     return render_template('base.html')
 
 def create_app(test_config=None):
