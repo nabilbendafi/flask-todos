7603472434ec3e5d325a7d987cf96fdc6910f0d7
Nabil BENDAFI <nabil@bendafi.fr>
2021-06-16 21:03:18 +0200

    feat: prompt for db passwords

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -3,3 +3,10 @@ variable "docker_image" {
   default     = "gcr.io/model-hexagon-316719/flask-todos"
 }
 
+variable "db_password" {
+  description = "PostgreSQL superuser password"
+}
+
+variable "db_application_password" {
+  description = "PostgreSQL Flask application password"
+}
