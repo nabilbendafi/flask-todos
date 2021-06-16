a3be55625c48155bd2f872c14849dbbd4c6d2a73
Nabil BENDAFI <nabil@bendafi.fr>
2021-06-16 21:09:37 +0200

    fix: already exist user

--- deploy/files/init-user-db.sh
+++ deploy/files/init-user-db.sh
@@ -3,11 +3,13 @@ set -e
 
 # "CREATE DATABASE IF NOT EXISTS" hack (https://zaiste.net/databases/postgresql/howtos/create-database-if-not-exists/)
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-_SQL_
-SELECT 'CREATE DATABASE todos'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'todos')\gexec
+    SELECT 'CREATE DATABASE todos'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'todos')\gexec
 _SQL_
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-_SQL_
-    CREATE USER flask WITH PASSWORD '$POSTGRES_APP_PASSWORD';
-    GRANT ALL PRIVILEGES ON DATABASE todos TO flask ;
+    SELECT 'CREATE USER flask'
+    WHERE NOT EXISTS (SELECT FROM pg_user WHERE usename = 'flask')\gexec
+    ALTER USER flask WITH  PASSWORD '$POSTGRES_APP_PASSWORD'
+    GRANT ALL PRIVILEGES ON DATABASE todos TO flask
 _SQL_
